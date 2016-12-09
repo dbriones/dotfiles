c5062fa747c6bee0be77f5f377fa621323f18245
Dante Briones <dante@cochiva.com>
2016-12-09 13:29:10 -0800

    Update prompt
    - extract meaningful names for font faces
    - deduplicate git status prompt

--- bash/.bash.d/prompt.bash
+++ bash/.bash.d/prompt.bash
@@ -3,22 +3,25 @@
 
 # taken from https://github.com/square/dotfiles.git
 
-RED="\[\033[0;31m\]"
-BROWN="\[\033[0;33m\]"
-CYAN="\[\e[01;36m\]"
-GREEN="\[\e[01;32m\]"
-GREY="\[\033[0;97m\]"
-BLUE="\[\033[0;34m\]"
-PS_CLEAR="\[\033[0m\]"
-SCREEN_ESC="\[\033k\033\134\]"
+NORM_RED="\[\e[0;31m\]"
+NORM_BROWN="\[\e[0;33m\]"
+NORM_GREY="\[\e[0;97m\]"
+NORM_BLUE="\[\e[0;34m\]"
+
+BOLD_CYAN="\[\e[01;36m\]"
+BOLD_GREEN="\[\e[01;32m\]"
+BOLD_YELLOW="\[\e[01;33m\]"
+
+PS_CLEAR="\[\e[0m\]"
+SCREEN_ESC="\[\ek\e\134\]"
 
 if [ "$LOGNAME" = "root" ]; then
-    COLOR1="${RED}"
-    COLOR2="${BROWN}"
+    COLOR1="${NORM_RED}"
+    COLOR2="${NORM_BROWN}"
     P="#"
 else
-    COLOR1="${BLUE}"
-    COLOR2="${BROWN}"
+    COLOR1="${NORM_BLUE}"
+    COLOR2="${NORM_BROWN}"
     P="\$"
 fi
 
@@ -35,14 +38,28 @@ prompt_compact() {
 }
 
 prompt_color() {
-    PS1="[${COLOR1}\u${GREY}@${COLOR2}\h${GREY}:${COLOR1}\W${GREY}]${COLOR2}$P${PS_CLEAR} "
-    PS2="\[;1m\]continue \[m\]> "
+    PS1="[${COLOR1}\u${NORM_GREY}@${COLOR2}\h${NORM_GREY}:${COLOR1}\W${NORM_GREY}]${COLOR2}$P${PS_CLEAR} "
+    PS2="${NORM_RED}continue ${PS_CLEAR}> "
 }
+
+# see: http://stufftohelpyouout.blogspot.com/2010/01/show-name-of-git-branch-in-prompt.html
+# see also: http://superuser.com/questions/31744/how-to-get-git-completion-bash-to-work-on-mac-os-x
+# see also: http://stackoverflow.com/questions/347901/what-are-your-favorite-git-features-or-tricks
+# This assumes you have installed Homebrew ( http://github.com/mxcl/homebrew )
+# and then installed Git via Homebrew with the default installation location:
+# ruby -e "$(curl -fsS http://gist.github.com/raw/323731/install_homebrew.rb)"
+# brew install wget
+# brew install git
+# brew update
+
 
 prompt_color_git() {
-    PS1="${GREEN}\u@\h${CYAN} \w \$(__git_ps1 '(%s) ')$P${PS_CLEAR} "
-    PS2="\[;1m\]continue \[m\]> "
+  if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]
+  then
+    . /usr/local/etc/bash_completion.d/git-completion.bash;
+    PS1="${BOLD_GREEN}\h${BOLD_CYAN} \W${BOLD_YELLOW}\$(__git_ps1)${PS_CLEAR} $ "
+    PS2="${NORM_RED}continue ${PS_CLEAR}> "
+  fi
 }
 
-# Use the color prompt by default when interactive
-prompt_color
+prompt_color_git
