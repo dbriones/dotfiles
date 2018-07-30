f729d8c09668da505d2e5475119e7dff6264a65c
Dante Briones <dante@cochiva.com>
2018-07-30 14:27:55 -0700

    reformat path and add Atom package bin directory

--- bash/.bash.d/path.bash
+++ bash/.bash.d/path.bash
@@ -1,1 +1,8 @@
-export PATH=/usr/local/bin:/usr/local/go/bin:/usr/local/opt/openssl/bin:$PATH:/usr/local/sbin:$HOME/bin
+export PATH=/usr/local/bin\
+:/usr/local/go/bin\
+:/usr/local/opt/openssl/bin\
+:$PATH\
+:/usr/local/sbin\
+:$HOME/bin\
+:/Applications/Atom.app/Contents/Resources/app/apm/bin\
+:/Applications/Atom.app/Contents/Resources/app/apm/node_modules/.bin
